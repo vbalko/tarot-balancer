fe7d850706c6cbe1a30701ffb183bba7f46e72bd
Vlado Balko <vbalko@gmail.com>
2022-01-19 00:35:40 +0100

    animation

--- src/lib/components/Balancer/Dashboard.svelte
+++ src/lib/components/Balancer/Dashboard.svelte
@@ -1,6 +1,9 @@
 <script>
 	import { provider, signer, signerAddress } from 'svelte-ethers-store';
 	import { ethers } from 'ethers';
+    import { fade, fly, blur, draw } from 'svelte/transition';
+    import { flip } from 'svelte/animate';
+
 	import {
 		Col,
 		Container,
@@ -55,10 +58,13 @@ NavItem
         </Col>
     </Row>
 	<Row   cols={{ lg: 3, md: 2, sm: 1 }}>
-		{#each dataToShow as vault}
-			<Col class="gy-2">
-				<VaultCard data={vault} />
+		{#each dataToShow as vault (vault.contract_address)}
+            <div animate:flip={{duration:800}} in:fly={{ duration:2500, x: 100, y: 150}} out:fly={{duration:400, x:100, y:150}}>
+                 
+                <Col class="gy-2" >				
+                    <VaultCard data={vault} />
 			</Col>
+            </div>
 		{/each}
 	</Row>
 </Container>
